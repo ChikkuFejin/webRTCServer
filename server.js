6aac8fc2f6eb6697cc09c2aa91076c2674d3bb0a
Fejin <rpf.fejin@gmail.com>
2023-09-25 09:50:53 +0530

    updated

--- server.js
+++ server.js
@@ -3,15 +3,72 @@ const http = require("http");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 const twilio = require("twilio");
+const fs = require('fs');
+const path = require('path');
+var bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+
 
 const PORT = process.env.PORT || 5002;
 const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(fileUpload());
 
 let connectedUsers = [];
 let rooms = [];
+
+app.use(express.static(path.join(__dirname, 'recordings')));
+
+const recordingsDirectory = path.join(__dirname, 'recordings');
+
+if (!fs.existsSync(recordingsDirectory)) {
+  fs.mkdirSync(recordingsDirectory);
+}
+
+app.post('/api/upload', (req, res) => {
+  const uploadedFile = req.files.video;
+
+  if (uploadedFile) {
+  
+    const fileName = uuidv4() + '.webm'; // Generate a unique file name
+    const filePath = path.join(recordingsDirectory, fileName);
+
+  
+    uploadedFile.mv(filePath, (err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      // File upload was successful
+      return res.json({ message: 'File uploaded successfully' });
+    });
+  } else {
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+});
+
+// app.post('/api/upload', (req, res) => {
+
+//   console.log('Uploading' ,req.files)
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+
+//   const videoFile = req.files.video;
+//   const fileName = uuidv4() + '.webm'; // Generate a unique file name
+//   const filePath = path.join(recordingsDirectory, fileName);
+
+//   videoFile.mv(filePath, (err) => {
+//     if (err) {
+//       return res.status(500).send(err);
+//     }
+//     res.send('Video uploaded and stored successfully.');
+//   });
+// });
 
 
 app.get("/api/room-exists/:roomId", (req, res) => {
@@ -33,7 +90,7 @@ app.get("/api/room-exists/:roomId", (req, res) => {
 
 app.get("/api/get-turn-credentials", (req, res) => {
   const accountSid = "AC2f5b06cac74c268ead9f2cfe6e615117";
-  const authToken = "0fff27c6810ac4bb3d63e6eabbad24b9";
+  const authToken = "37fa3b4085b0b9c31601b9090913e89a";
 
   const client = twilio(accountSid, authToken);
 
@@ -41,6 +98,8 @@ app.get("/api/get-turn-credentials", (req, res) => {
   try {
     client.tokens.create().then((token) => {
       res.send({ token });
+    }).catch(err => {
+      console.log("error occurred when fetching turn server credentials",err);
     });
   } catch (err) {
     console.log("error occurred when fetching turn server credentials");
@@ -57,6 +116,7 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
+
   console.log(`user connected ${socket.id}`);
 
   socket.on("create-new-room", (data) => {
